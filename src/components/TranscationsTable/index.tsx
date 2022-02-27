0c793b0a3125ed385386ccc61285b944557705bc
Guilherme Ribeiro <augusto.gui.2208@gmail.com>
2022-02-27 20:13:25 -0300

    contextos e hooks

--- src/components/TranscationsTable/index.tsx
+++ src/components/TranscationsTable/index.tsx
@@ -1,24 +1,8 @@
-import { useEffect, useState } from 'react'
-import { api } from '../../services/api'
+import { useTransactions } from '../../hooks/useTransactions'
 import { Container } from './styles'
 
-interface transaction {
-  id: number
-  title: string
-  amount: number
-  type: string
-  category: string
-  createAt: string
-}
-
 export function TranscationsTable() {
-  const [transactions, setTransactions] = useState<transaction[]>([])
-
-  useEffect(() => {
-    api
-      .get('transactions')
-      .then(response => setTransactions(response.data.transactions))
-  }, [])
+  const { transactions } = useTransactions()
 
   return (
     <Container>
